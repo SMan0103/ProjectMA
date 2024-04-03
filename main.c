e1be9662ccc83fb686d6bc4b990ef0ade26140bd
Emil Pedersen <steamman0103@gmail.com>
2024-04-03 15:39:32 +0200

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7 ");
+
+    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+
+    printf("Last Command: \n");
+    printf("Message: \n");
+    printf("Input > \n");
     return 0;
 }
